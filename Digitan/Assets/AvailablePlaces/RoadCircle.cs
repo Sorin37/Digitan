f9bd1782e6173ccc74dc3205c901b6200f20c944
Sorin37 <glizard.glizard@yahoo.com>
2022-12-12 20:09:27 +0200

    Implemented collision detection for circles

--- Digitan/Assets/AvailablePlaces/RoadCircle.cs
+++ Digitan/Assets/AvailablePlaces/RoadCircle.cs
@@ -27,10 +27,23 @@ public class RoadCircle : MonoBehaviour
         //_renderer.material.color = Color.white;
         if (!isCube)
         {
+            Debug.Log("xd");
+
+            var Colliders = Physics2D.OverlapCircleAll(
+                transform.position,
+                2
+            );
+
+            for (int i = 0; i < Colliders.Length; i++) { 
+                print(Colliders[i].gameObject.transform.name);
+            }
+
             GameObject roadObject = Instantiate(road, transform.position, Quaternion.Euler(90, 0, 0));
             roadObject.GetComponent<RoadCircle>().isCube = true;
             Destroy(this);
-            Camera.main.cullingMask = Camera.main.cullingMask & ~(1 << LayerMask.NameToLayer("Road Circle"));
+
+            ////make the road circles invisible
+            //Camera.main.cullingMask = Camera.main.cullingMask & ~(1 << LayerMask.NameToLayer("Road Circle"));
         }
     }
 }
