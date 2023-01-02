fc4ce40b406c01d3c066559d47f5cb487f19d0c4
Sorin37 <glizard.glizard@yahoo.com>
2023-01-02 15:19:38 +0200

    Red numbers won't appear next to each other anymore

--- Digitan/Assets/Scripts/NumbersGrid.cs
+++ Digitan/Assets/Scripts/NumbersGrid.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.UIElements;
 
 public class NumbersGrid : MonoBehaviour
 {
@@ -41,10 +42,6 @@ public class NumbersGrid : MonoBehaviour
             Debug.LogError("Error: One of the prefabs is not assigned");
             return;
         }
-        else
-        {
-            hexSize = resourceGrid.GetComponent<GameGrid>().hexSize;
-        }
 
     }
 
@@ -56,6 +53,8 @@ public class NumbersGrid : MonoBehaviour
 
     public void CreateGrid()
     {
+        hexSize = resourceGrid.GetComponent<GameGrid>().hexSize;
+
         numbersGrid = new GameObject[5][];
         numbersGrid[0] = new GameObject[3];
         numbersGrid[1] = new GameObject[4];
@@ -63,6 +62,28 @@ public class NumbersGrid : MonoBehaviour
         numbersGrid[3] = new GameObject[4];
         numbersGrid[4] = new GameObject[3];
 
+        instantiateThePieces();
+    }
+
+    private bool isRedNumberNearby(Vector3 position)
+    {
+        var colliders = Physics.OverlapSphere(
+            position,
+            hexSize / 2 + 2,
+            (int)Mathf.Pow(2, LayerMask.NameToLayer("Number"))
+        );
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.name == "6" || colliders[i].gameObject.name == "8")
+                return true;
+        }
+
+        return false;
+    }
+
+    private void instantiateThePieces()
+    {
         List<GameObject> numbersPool = new List<GameObject> {
             number2,
             number3,  number3,
@@ -76,7 +97,6 @@ public class NumbersGrid : MonoBehaviour
             number12
         };
 
-        //Make the grid
         for (int x = 0; x <= numbersGrid.Length / 2; x++)
         {
             for (int y = 0; y < numbersGrid[x].Length; y++)
@@ -84,45 +104,124 @@ public class NumbersGrid : MonoBehaviour
                 if (resourceGrid.GetComponent<GameGrid>().gameGrid[x][y].name == "Desert")
                     continue;
 
+                Vector3 position = new Vector3(y * hexSize - x * hexSize / 2,
+                                               0.1f,
+                                               -x * hexSize * 3 / 4);
+
                 int randomIndex = Random.Range(0, numbersPool.Count);
                 GameObject number = numbersPool[randomIndex];
                 numbersPool.RemoveAt(randomIndex);
 
+                if (number.name == "6" || number.name == "8")
+                {
+                    if (isRedNumberNearby(position))
+                    {
+                        numbersPool.Add(number);
+                        --y;
+                        continue;
+                    }
+                }
+
                 numbersGrid[x][y] = Instantiate(
                     number,
-                    new Vector3(y * hexSize - x * hexSize / 2,
-                                0.1f,
-                                -x * hexSize * 3 / 4),
+                    position,
                     Quaternion.Euler(-90, -90, 90)
                     );
 
                 numbersGrid[x][y].transform.parent = transform;
-                numbersGrid[x][y].gameObject.name = "NumberPiece(" + x.ToString() + ", " + y.ToString() + ")";
+                numbersGrid[x][y].gameObject.name = number.name;
             }
         }
 
+
         for (int x = numbersGrid.Length / 2 + 1; x < numbersGrid.Length; x++)
         {
+            int tries = 0;
+
             for (int y = 0; y < numbersGrid[x].Length; y++)
             {
                 if (resourceGrid.GetComponent<GameGrid>().gameGrid[x][y].name == "Desert")
                     continue;
 
+                Vector3 position = new Vector3(y * hexSize + x * hexSize / 2 - hexSize * 2,
+                                0.1f,
+                                -x * hexSize * 3 / 4);
+
                 int randomIndex = Random.Range(0, numbersPool.Count);
                 GameObject number = numbersPool[randomIndex];
                 numbersPool.RemoveAt(randomIndex);
 
+                if (number.name == "6" || number.name == "8")
+                {
+                    if (isRedNumberNearby(position))
+                    {
+                        if (++tries == 10)
+                        {
+                            findAvailableSpace(x, y, position, number);
+                            tries = 0;
+                            continue;
+                        }
+
+                        numbersPool.Add(number);
+                        --y;
+                        continue;
+                    }
+                }
+
                 numbersGrid[x][y] = Instantiate(
                     number,
-                    new Vector3(y * hexSize + x * hexSize / 2 - hexSize * 2,
-                                0.1f,
-                                -x * hexSize * 3 / 4),
+                    position,
                     Quaternion.Euler(-90, -90, 90)
                 );
 
                 numbersGrid[x][y].transform.parent = transform;
-                numbersGrid[x][y].gameObject.name = "NumberPiece(" + x.ToString() + ", " + y.ToString() + ")";
+                numbersGrid[x][y].gameObject.name = number.name;
             }
         }
+    }
+
+    void findAvailableSpace(int x, int y, Vector3 position, GameObject number)
+    {
+        print("It happened");
+
+        List<(int, int)> availableSpaces= new List<(int, int)> ();
+
+        for(int i=0; i < numbersGrid.Length; i++)
+        {
+            for(int j = 0; j < numbersGrid[i].Length; j++)
+            {
+                if (numbersGrid[i][j] != null)
+                {
+                    if (!isRedNumberNearby(numbersGrid[i][j].gameObject.transform.position))
+                    {
+                        availableSpaces.Add((i, j));
+                    }
+                }
+            }
+        }
+
+        int randomIndex = Random.Range(0, availableSpaces.Count);
+        int spaceI = availableSpaces[randomIndex].Item1;
+        int spaceJ = availableSpaces[randomIndex].Item2;
+        Vector3 availableSpacePosition = numbersGrid[spaceI][spaceJ].gameObject.transform.position;
+
+        //move the old number
+        numbersGrid[x][y] = Instantiate(
+                    numbersGrid[spaceI][spaceJ].gameObject,
+                    position,
+                    Quaternion.Euler(-90, -90, 90)
+        );
+        numbersGrid[x][y].transform.parent = transform;
+        numbersGrid[x][y].gameObject.name = numbersGrid[spaceI][spaceJ].gameObject.name;
+
+        //instantiate the new number
+        Destroy(numbersGrid[spaceI][spaceJ]);
+        numbersGrid[spaceI][spaceJ] = Instantiate(
+                    number,
+                    availableSpacePosition,
+                    Quaternion.Euler(-90, -90, 90)
+                );
+        numbersGrid[spaceI][spaceJ].transform.parent = transform;
+        numbersGrid[spaceI][spaceJ].gameObject.name = number.name;
     }
 }
