e2d2e70d8f050fe372482c881498be1efbdeca36
Sorin37 <glizard.glizard@yahoo.com>
2023-01-28 18:16:09 +0200

    Added hand cards sprites

--- Digitan/Assets/Scripts/NumbersGrid.cs
+++ Digitan/Assets/Scripts/NumbersGrid.cs
@@ -1,10 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
-using UnityEngine.Assertions;
-using UnityEngine.UIElements;
 
 public class NumbersGrid : MonoBehaviour
 {
