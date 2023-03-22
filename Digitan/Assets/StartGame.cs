d8d6df44e334b3686771b33c80a6398a14c0458f
Sorin37 <glizard.glizard@yahoo.com>
2023-03-22 23:07:15 +0200

    Back to the game grid now

--- Digitan/Assets/StartGame.cs
+++ Digitan/Assets/StartGame.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class StartGame : NetworkBehaviour
 {
-    [SerializeField] private Transform gameGrid;
+    [SerializeField] private GameObject gameGrid;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +28,7 @@ public class StartGame : NetworkBehaviour
     [ServerRpc]
     public void FirstStepTowardsSuccessServerRpc()
     {
-        Transform _gameGrid = Instantiate(gameGrid);
+        GameObject _gameGrid = Instantiate(gameGrid);
         _gameGrid.GetComponent<NetworkObject>().Spawn();
         //_gameGrid.GetComponent<GameGrid>().CreateGrid();
     }
