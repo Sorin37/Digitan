7151ba8fda0ca44729bd360b9d720b9a965024ac
Sorin37 <glizard.glizard@yahoo.com>
2023-04-01 10:11:58 +0300

    Place smth only if the player has the resources for it

--- Digitan/Assets/Scripts/ButtonManager.cs
+++ Digitan/Assets/Scripts/ButtonManager.cs
@@ -12,15 +12,11 @@ public class ButtonManager : MonoBehaviour
 {
     [SerializeField] private Button settlementButton;
     [SerializeField] private Button roadButton;
-    [SerializeField] private Button diceButton;
     [SerializeField] private Button endTurnButton;
-    public GameObject gameGrid;
-    public GameObject player;
 
     private void Awake()
     {
         initEndTurnButton();
-        initDiceButton();
         initSettlementButton();
         initRoadButton();
     }
@@ -35,14 +31,14 @@ public class ButtonManager : MonoBehaviour
                 return;
             }
 
-            //if (hasRoadResources())
-            //{
-            Camera.main.cullingMask = Camera.main.cullingMask | (1 << LayerMask.NameToLayer("Road Circle"));
-            //}
-            //else
-            //{
-            //    print("You don't have enough resources for a road!");
-            //}
+            if (hasRoadResources())
+            {
+                Camera.main.cullingMask = Camera.main.cullingMask | (1 << LayerMask.NameToLayer("Road Circle"));
+            }
+            else
+            {
+                print("You don't have enough resources for a road!");
+            }
         });
     }
     private void initSettlementButton()
@@ -54,73 +50,48 @@ public class ButtonManager : MonoBehaviour
                 print("nu e runda mea :/");
                 return;
             }
-            //if (hasSettlementResources())
-            //{
-            Camera.main.cullingMask = Camera.main.cullingMask | (1 << LayerMask.NameToLayer("Settlement Circle"));
-            //}
-            //else
-            //{
-            //    print("You don't have enough resources for a settlement!");
-            //}
-        });
-    }
-    private void initDiceButton()
-    {
-        diceButton.onClick.AddListener(() =>
-        {
-            if (GetHostPlayer().GetComponent<Player>().currentPlayerTurn.Value == (int)NetworkManager.Singleton.LocalClientId)
+
+            if (hasSettlementResources())
             {
-                print("nu e runda mea :/");
-                return;
+                Camera.main.cullingMask = Camera.main.cullingMask | (1 << LayerMask.NameToLayer("Settlement Circle"));
             }
-
-            //var resourcesDict = gameGrid.GetComponent<GameGrid>().resourcesDict;
-            //var playerHand = gameGrid.GetComponent<GameGrid>().playerHand;
-
-            //if (resourcesDict.ContainsKey((dice1 + dice2).ToString()))
-            //{
-            //    print((dice1 + dice2).ToString() + " You got: " + String.Join(",", resourcesDict[(dice1 + dice2).ToString()].ToArray()));
-
-            //    foreach (String resource in resourcesDict[(dice1 + dice2).ToString()])
-            //    {
-            //        playerHand[resource]++;
-            //    }
-
-            //    displayCards();
-            //}
-            //else
-            //{
-            //    print((dice1 + dice2).ToString() + " You got nothing!");
-            //}
+            else
+            {
+                print("You don't have enough resources for a settlement!");
+            }
         });
     }
+
     bool hasSettlementResources()
     {
-        //var playerHand = gameGrid.GetComponent<GameGrid>().playerHand;
-        //if (playerHand["Brick Resource"] > 0 &&
-        //    playerHand["Grain Resource"] > 0 &&
-        //    playerHand["Lumber Resource"] > 0 &&
-        //    playerHand["Wool Resource"] > 0)
-        //{
-        //    playerHand["Brick Resource"]--;
-        //    playerHand["Grain Resource"]--;
-        //    playerHand["Lumber Resource"]--;
-        //    playerHand["Wool Resource"]--;
-        //    return true;
-        //}
+        var playerHand = GetMyPlayer().GetComponent<Player>().playerHand;
+        if (playerHand["Brick Resource"] > 0 &&
+            playerHand["Grain Resource"] > 0 &&
+            playerHand["Lumber Resource"] > 0 &&
+            playerHand["Wool Resource"] > 0)
+        {
+            playerHand["Brick Resource"]--;
+            playerHand["Grain Resource"]--;
+            playerHand["Lumber Resource"]--;
+            playerHand["Wool Resource"]--;
+            return true;
+        }
+
         return false;
     }
 
     bool hasRoadResources()
     {
-        //var playerHand = gameGrid.GetComponent<GameGrid>().playerHand;
-        //if (playerHand["Brick Resource"] > 0 &&
-        //    playerHand["Lumber Resource"] > 0)
-        //{
-        //    playerHand["Brick Resource"]--;
-        //    playerHand["Lumber Resource"]--;
-        //    return true;
-        //}
+        var playerHand = GetMyPlayer().GetComponent<Player>().playerHand;
+
+        if (playerHand["Brick Resource"] > 0 &&
+            playerHand["Lumber Resource"] > 0)
+        {
+            playerHand["Brick Resource"]--;
+            playerHand["Lumber Resource"]--;
+            return true;
+        }
+
         return false;
     }
 
@@ -149,5 +120,18 @@ public class ButtonManager : MonoBehaviour
             print("a fost runda mea hehe");
             GetHostPlayer().GetComponent<Player>().PassTurnServerRpc();
         });
+    }
+
+    private GameObject GetMyPlayer()
+    {
+        var players = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (var p in players)
+        {
+            if (p.GetComponent<Player>().IsOwner)
+                return p;
+        }
+
+        return null;
     }
 }
