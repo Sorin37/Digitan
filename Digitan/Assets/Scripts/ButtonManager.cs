c24358eaf820e79d97e4dab6a5b2e671842969e8
Sorin37 <glizard.glizard@yahoo.com>
2023-04-18 17:11:57 +0300

    Created the City Place model

--- Digitan/Assets/Scripts/ButtonManager.cs
+++ Digitan/Assets/Scripts/ButtonManager.cs
@@ -30,12 +30,10 @@ public class ButtonManager : MonoBehaviour
     {
         endTurnButton.onClick.AddListener(() =>
         {
-            if (GetHostPlayer().GetComponent<Player>().currentPlayerTurn.Value != (int)NetworkManager.Singleton.LocalClientId)
+            if (!IsMyTurn())
             {
-                print("nu e runda mea :/");
                 return;
             }
-            print("a fost runda mea hehe");
             GetHostPlayer().GetComponent<Player>().PassTurnServerRpc();
         });
     }
@@ -43,13 +41,12 @@ public class ButtonManager : MonoBehaviour
     {
         settlementButton.onClick.AddListener(() =>
         {
-            if (GetHostPlayer().GetComponent<Player>().currentPlayerTurn.Value != (int)NetworkManager.Singleton.LocalClientId)
+            if (!IsMyTurn())
             {
-                print("nu e runda mea :/");
                 return;
             }
 
-            if (hasSettlementResources())
+            if (HasSettlementResources())
             {
                 Camera.main.cullingMask = Camera.main.cullingMask | (1 << LayerMask.NameToLayer("Settlement Circle"));
             }
@@ -63,13 +60,12 @@ public class ButtonManager : MonoBehaviour
     {
         roadButton.onClick.AddListener(() =>
         {
-            if (GetHostPlayer().GetComponent<Player>().currentPlayerTurn.Value != (int)NetworkManager.Singleton.LocalClientId)
+            if (!IsMyTurn())
             {
-                print("nu e runda mea :/");
                 return;
             }
 
-            if (hasRoadResources())
+            if (HasRoadResources())
             {
                 Camera.main.cullingMask = Camera.main.cullingMask | (1 << LayerMask.NameToLayer("Road Circle"));
             }
@@ -83,6 +79,10 @@ public class ButtonManager : MonoBehaviour
     {
         tradeButton.onClick.AddListener(() =>
         {
+            if (!IsMyTurn())
+            {
+                return;
+            }
             ResetTrade();
             tradeCanvas.gameObject.SetActive(true);
         });
@@ -92,10 +92,18 @@ public class ButtonManager : MonoBehaviour
         cityButton.onClick.AddListener(() =>
         {
             print("se fac orase");
+            //if (HasCityResources())
+            //{
+            Camera.main.cullingMask = Camera.main.cullingMask | (1 << LayerMask.NameToLayer("City Place"));
+            //}
+            //else
+            //{
+            //    print("You don't have enough resources for a road!");
+            //}
         });
     }
 
-    bool hasSettlementResources()
+    bool HasSettlementResources()
     {
         var playerHand = GetMyPlayer().GetComponent<Player>().playerHand;
         if (playerHand["Brick Resource"] > 0 &&
@@ -116,7 +124,7 @@ public class ButtonManager : MonoBehaviour
         return false;
     }
 
-    bool hasRoadResources()
+    bool HasRoadResources()
     {
         var playerHand = GetMyPlayer().GetComponent<Player>().playerHand;
 
@@ -124,6 +132,23 @@ public class ButtonManager : MonoBehaviour
         {
             playerHand["Brick Resource"]--;
             playerHand["Lumber Resource"]--;
+
+            GetMyPlayer().GetComponent<Player>().UpdateHand();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool HasCityResources()
+    {
+        var playerHand = GetMyPlayer().GetComponent<Player>().playerHand;
+
+        if (playerHand["Ore Resource"] > 2 && playerHand["Grain Resource"] > 1)
+        {
+            playerHand["Ore Resource"] -= 3;
+            playerHand["Grain Resource"] -= 2;
 
             GetMyPlayer().GetComponent<Player>().UpdateHand();
 
@@ -180,5 +205,10 @@ public class ButtonManager : MonoBehaviour
         GetHostPlayer().GetComponent<Player>().nrOfDeclinedTrades = 0;
 
         tradeManager.DrawDicts();
+    }
+
+    private bool IsMyTurn()
+    {
+        return GetHostPlayer().GetComponent<Player>().currentPlayerTurn.Value == (int)NetworkManager.Singleton.LocalClientId;
     }
 }
