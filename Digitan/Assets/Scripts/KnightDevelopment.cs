96ba246ec11298bfb745e917eae7b56e0a8ac7b9
Sorin37 <glizard.glizard@yahoo.com>
2023-05-27 13:55:01 +0300

    Removing the development after using it

---
+++ Digitan/Assets/Scripts/KnightDevelopment.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class KnightDevelopment : MonoBehaviour
+{
+    [SerializeField] private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("K am un cavaler");
+        InitializeButton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitializeButton()
+    {
+        button.onClick.AddListener(() =>
+        {
+            //fidn the deck
+            var deckGroup = Resources.FindObjectsOfTypeAll<DeckGroup>()[0];
+
+            var deckName = "KnightDeck";
+
+            GameObject deck = null;
+
+            foreach (Transform child in deckGroup.transform)
+            {
+                if (child.gameObject.name == deckName)
+                {
+                    deck = child.gameObject;
+                    break;
+                }
+            }
+
+            if (deck == null)
+            {
+                print("Didn't find the " + deckName + " after touching the card");
+                return;
+            }
+
+            //remove the card
+            Destroy(gameObject);
+
+            //resize the deck
+            var deckRectTransform = deck.transform as RectTransform;
+
+            float width = 0;
+
+            if (deckRectTransform.sizeDelta.x == 100)
+            {
+                width = -120;
+            }
+            else if (deckRectTransform.sizeDelta.x > 100)
+            {
+                width = -25;
+            }
+
+            deckRectTransform.sizeDelta = new Vector2(deckRectTransform.sizeDelta.x + width, deckRectTransform.sizeDelta.y);
+        });
+    }
+}
