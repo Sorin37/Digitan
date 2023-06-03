514c45ecae31ed1d09bc15a375dd86edd316e87f
Sorin37 <glizard.glizard@yahoo.com>
2023-06-03 15:38:13 +0300

    I am able to determine the max value of the spot, but not the spot

--- Digitan/Assets/Scripts/TipsManager.cs
+++ Digitan/Assets/Scripts/TipsManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class TipsManager : MonoBehaviour
 {
     private float HexSize = 5;
-    [SerializeField] private GameObject testPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,36 +23,54 @@ public class TipsManager : MonoBehaviour
     {
         List<int> spacesPerRow = new List<int> { 3, 4, 4, 5, 5, 6, 6, 5, 5, 4, 4, 3 };
 
+        int max = 0;
+
         for (int x = 0; x < spacesPerRow.Count; x++)
         {
             for (int y = 0; y < spacesPerRow[x]; y++)
             {
-                GameObject go = null;
+                int value = 0;
+
                 if (x < spacesPerRow.Count / 2)
                 {
-                    go = Instantiate(
-                        testPrefab,
-                        new Vector3(
-                            y * HexSize - x * HexSize / 2 + Mathf.Floor(x / 2) * HexSize / 2,
-                            0.5f,
-                            -Mathf.Floor(x / 2) * HexSize * 3 / 4 + HexSize / 2 - (x % 2) * HexSize / 4),
-                        Quaternion.Euler(90, 0, 0)
+                    value = CollidersValue(new Vector3(
+                        y * HexSize - x * HexSize / 2 + Mathf.Floor(x / 2) * HexSize / 2,
+                        0.5f,
+                        -Mathf.Floor(x / 2) * HexSize * 3 / 4 + HexSize / 2 - (x % 2) * HexSize / 4)
                     );
                 }
                 else
                 {
-                    go = Instantiate(
-                       testPrefab,
-                       new Vector3(
+                    value = CollidersValue(new Vector3(
                            y * HexSize + (x - spacesPerRow.Count / 2) * HexSize / 2 - Mathf.Floor(x / 2) * HexSize / 2,
                            0.5f,
-                           -Mathf.Floor(x / 2) * HexSize * 3 / 4 + HexSize / 2 - (x % 2) * HexSize / 4),
-                       Quaternion.Euler(90, 0, 0)
+                           -Mathf.Floor(x / 2) * HexSize * 3 / 4 + HexSize / 2 - (x % 2) * HexSize / 4)
                     );
                 }
 
-                go.gameObject.name = "TestPrefab(" + (x + 1).ToString() + ", " + (y + 1).ToString() + ")";
+                max = max < value ? value : max;
             }
         }
+
+        print("Cel mai bun spot are: " + max);
+    }
+
+    private int CollidersValue(Vector3 pos)
+    {
+
+        int value = 0;
+
+        var colliders = Physics.OverlapSphere(
+            pos,
+            2.5f,
+            (int)Mathf.Pow(2, LayerMask.NameToLayer("Number"))
+        );
+
+        foreach (var collider in colliders)
+        {
+            value += collider.GetComponent<Number>().frequency;
+        }
+
+        return value;
     }
 }
